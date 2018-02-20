174dc4a75371890af7827d90548cda641b214560
Sunil Bhujle <Sunil.Bhujle@LighthouseSoftware.com>
2018-02-20 14:33:07 -0600

    - Initial solution - About Controller -

---
+++ src/CVT.Galvanize.Api/Controllers/AboutController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CVT.Galvanize.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class AboutController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public async Task<AboutModel> Get()
+        {
+            return await Task.Run(() => new AboutModel
+            {
+                Name = "CVT Galvanize",
+                Version = "0.0.1"
+            });
+        }
+    }
+
+    public sealed class AboutModel
+    {
+        public string Version { get; set; }
+
+        public string Name { get; set; }
+    }
+}
